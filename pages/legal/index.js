7085acef97bf36be63b88c8d916df24923f90b8f
Tactikerl <61546873+Tactikerl@users.noreply.github.com>
2023-07-13 14:22:44 +0200

    testing fields for cms

---
+++ pages/legal/index.js
@@ -0,0 +1,30 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import ReactMarkdown from 'react-markdown'
+import Link from 'next/link'
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
+
+export default function Home({ frontmatter, markdown}) {
+  return (    <div>
+    <Head>
+      <title>Demo Blog | {frontmatter.title}</title>
+    </Head>
+    <h1>{frontmatter.title}</h1>
+    <span>{frontmatter.date}</span>
+    <hr />
+    <ReactMarkdown>
+      {markdown}
+    </ReactMarkdown>
+  </div>)
+}
+
+export async function getStaticProps() {
+    const fileContent = matter(fs.readFileSync(`./content/pages/legal/index.md`, 'utf8'))
+    let frontmatter = fileContent.data
+    const markdown = fileContent.content
+  
+    return {
+      props: { frontmatter, markdown }
+    }
+  }
