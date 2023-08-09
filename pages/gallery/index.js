d928ab21150b01004fe3730067857f36c695f580
Tactikerl <61546873+Tactikerl@users.noreply.github.com>
2023-08-09 19:38:58 +0200

    added gallery ui for the cms

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -3,7 +3,7 @@ import matter from 'gray-matter'
 import ReactMarkdown from 'react-markdown'
 import Link from 'next/link'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 export default function Home({ frontmatter, markdown }) {
   return (
