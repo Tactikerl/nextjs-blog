62e5c75e4a351b5c385e915d548ace393e675969
Tactikerl <61546873+Tactikerl@users.noreply.github.com>
2024-01-17 14:32:37 +0100

    Changed image gallery css, each img div take 250px each. added image loader to the gallery background. set text overflow to elipses so that images with alot of text dnot take more space than others. planning for iamge modal, will make the full image text visible in the modal

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -4,82 +4,12 @@ import ReactMarkdown from 'react-markdown'
 import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
+import ImageLoader from '../../components/ImageLoader'
 
 export default function Home({ frontmatter, markdown }) {
   return (
     <div className={styles['container']}>
-      <div className={styles['mexicanHedge']}>
-        <div className={styles['mexicanWall']}>
-          <img
-            src="../images/radiospilling.png"
-            className={styles['mexicanImage-1']}
-          />
-          <img src="../images/barna.png" className={styles['mexicanImage-2']} />
-          <img src="../images/brev.png" className={styles['mexicanImage-3']} />
-          <img
-            src="../images/dansbrakka.png"
-            className={styles['mexicanImage-4']}
-          />
-          <img
-            src="../images/dansmengde.png"
-            className={styles['mexicanImage-5']}
-          />
-          <img src="../images/kaia.png" className={styles['mexicanImage-6']} />
-          <img
-            src="../images/medlemsliste.png"
-            className={styles['mexicanImage-7']}
-          />
-          <img src="../images/park.png" className={styles['mexicanImage-8']} />
-          <img
-            src="../images/poserer.png"
-            className={styles['mexicanImage-9']}
-          />
-          <img
-            src="../images/Instrumentbytte.png"
-            className={styles['mexicanImage-10']}
-          />
-          <img
-            src="../images/Snappy.png"
-            className={styles['mexicanImage-11']}
-          />
-          <img
-            src="../images/snappyinthewild.png"
-            className={styles['mexicanImage-12']}
-          />
-          <img
-            src="../images/snappyjenter.png"
-            className={styles['mexicanImage-13']}
-          />
-          <img
-            src="../images/snappypose.png"
-            className={styles['mexicanImage-14']}
-          />
-          <img
-            src="../images/snappypose2.png"
-            className={styles['mexicanImage-15']}
-          />
-          <img
-            src="../images/snappypractice.png"
-            className={styles['mexicanImage-16']}
-          />
-          <img
-            src="../images/Sture.png"
-            className={styles['mexicanImage-17']}
-          />
-          <img
-            src="../images/stureogodd.png"
-            className={styles['mexicanImage-18']}
-          />
-          <img
-            src="../images/tormod.png"
-            className={styles['mexicanImage-19']}
-          />
-          <img
-            src="../images/venner.png"
-            className={styles['mexicanImage-20']}
-          />
-        </div>
-      </div>
+      <ImageLoader numberOfImages={9} />
       <Head>
         <title>Sture & Tormod Halvorsens legat</title>
       </Head>
@@ -119,15 +49,14 @@ export default function Home({ frontmatter, markdown }) {
           {frontmatter.imageGallery.map((item, index) => (
             <li key={index}>
               <div className={styles['imageContainer']}>
+                <h3>{item.title}</h3>
                 <img
                   src={item.image}
                   className={styles['contentImg']}
                   alt={item.alt}
                 />
-              </div>
-              <div className={styles['textContainer']}>
-                <h3>{item.title}</h3>
-                <p>{item.text}</p>
+
+                <p className={styles['galleryP']}>{item.text}</p>
               </div>
             </li>
           ))}
